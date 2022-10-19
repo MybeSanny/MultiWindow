fbfa0fb4b26f4497c344ed94049433c9877653b7
Nattzzz <danillooktora81@gmail.com>
2022-10-19 11:22:07 +0700

    Multiwindow

---
+++ app/src/main/java/com/example/multiwindow/Berita.java
@@ -0,0 +1,17 @@
+package com.example.multiwindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class Berita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berita);
+       
+    }
+}
